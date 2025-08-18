2605871742b7027fb4dc4dbac3a5e11fa6b0810f
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2025-08-18 20:58:33 +0530

    Added GitHub OAuth Login

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ To start the app, run:
 yarn install
 yarn start
 ```
+
+---
+
+### Setup
+
+Create a new backstage app:
+```bash
+npx @backstage/create-app@latest
+```
+
+Start backstage:
+```bash
+yarn start
+```
+
+In the backend directory, add the GitHub auth provider:
+```bash
+yarn --cwd packages/backend add @backstage/plugin-auth-backend-module-github-provider
+```
--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Route } from 'react-router-dom';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
 import { apiDocsPlugin, ApiExplorerPage } from '@backstage/plugin-api-docs';
 import {
   CatalogEntityPage,
@@ -56,7 +57,18 @@ const app = createApp({
     });
   },
   components: {
-    SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
+    SignInPage: props => (
+      <SignInPage
+        {...props}
+        auto
+        provider={{
+          id: 'github-auth-provider',
+          title: 'GitHub',
+          message: 'Sign in using GitHub',
+          apiRef: githubAuthApiRef,
+        }}
+      />
+    ),
   },
 });
 
